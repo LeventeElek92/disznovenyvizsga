4c72ff74456904c1a3de10d16e25c5e27dc2cd8a
Levente Elek <elek.levente@gmail.com>
2024-04-11 12:06:20 +0200

    vizsga

---
+++ src/app/enviroments.ts
@@ -0,0 +1,13 @@
+export const Enviroments = {
+    production:false,
+    firebaseConfig : {
+        apiKey: "AIzaSyBbFuUtAobLttFTFxmZDvN5xmgmeUy2RzA",
+        authDomain: "vizsga0411.firebaseapp.com",
+        databaseURL: "https://vizsga0411-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "vizsga0411",
+        storageBucket: "vizsga0411.appspot.com",
+        messagingSenderId: "287572592094",
+        appId: "1:287572592094:web:eb4f8d4e99108d8f20494f",
+        measurementId: "G-0V7H61RS1Z"
+    }
+}
